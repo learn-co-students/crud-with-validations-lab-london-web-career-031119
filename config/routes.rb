bd51ced3a29dfd8b353418c12a265b9d4ec43660
Max Cantor <max@maxcantor.net>
2015-12-09 00:30:35 -0500

    Remove solution-only information.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,2 @@
 Rails.application.routes.draw do
-  get "/songs", to: "songs#index"
-  get "/songs/new", to: "songs#new", as: "new_song"
-  post "/songs", to: "songs#create"
-  get "/songs/:id", to: "songs#show", as: "song"
-  get "/songs/:id/edit", to: "songs#edit", as: "edit_song"
-  patch "/songs/:id", to: "songs#update"
-  delete "/songs/:id", to: "songs#destroy"
-
-  root "songs#index"
 end
