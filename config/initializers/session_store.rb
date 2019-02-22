824037c3702db7eab4ad6cbf21b18ee540638ed5
maxwellbenton <maxwellbenton@gmail.com>
2019-02-22 14:20:23 -0500

    rails app:update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_crud-with-validations-lab_session'
+Rails.application.config.session_store :cookie_store, key: '_crud_with_validations_lab_session'
